9dcf0c3018e480f971b423780ce851060e5ad06b
Vladislav Bauer <vbauer@yandex-team.ru>
2016-07-09 18:22:51 +0600

    Move options in separate module

---
+++ src/options.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const _ = require('lodash'),
+      joi = require('joi'),
+      qs = require('qs'),
+
+      REGEXP_CLIP_RECT = /^(\d*),(\d*),([1-9]\d*),([1-9]\d*)$/;
+
+
+/* Schemas */
+
+function createSchema() {
+    return joi.object().keys({
+        force: joi.boolean(),
+        url: joi.string().trim().required(),
+        agent: joi.string().trim(),
+        headers: joi.string().trim(),
+        delay: joi.number().integer().min(0),
+        format: joi.string().lowercase().trim().valid(
+            'jpeg', 'jpg', 'png', 'html', 'bmp', 'pdf', 'ppm', 'ico'),
+        engine: joi.string().lowercase().trim().valid(
+            'phantomjs', 'slimerjs'),
+        quality: joi.number().min(0).max(1),
+        width: joi.number().integer().min(1),
+        height: joi.number().integer().min(1),
+        clipRect: joi.string().trim().regex(REGEXP_CLIP_RECT),
+        zoom: joi.number().min(0),
+        selector: joi.string().trim(),
+        js: joi.boolean(),
+        images: joi.boolean(),
+        user: joi.string().trim(),
+        password: joi.string().trim(),
+        callback: joi.string().trim(),
+        cookies: joi.array().items(
+            joi.object().keys({
+                name: joi.string().required(),
+                value: joi.string().required(),
+                domain: joi.string(),
+                path: joi.string().required(),
+                httponly: joi.boolean(),
+                secure: joi.boolean(),
+                expires: joi.string()
+            })
+        )
+    });
+}
+
+
+/* Functions to parse options */
+
+function parseClipRect(cr) {
+    const params = (cr || '').match(REGEXP_CLIP_RECT);
+    if (params && (params.length === 5)) {
+        return {
+            top: parseInt(params[1]),
+            left: parseInt(params[2]),
+            width: parseInt(params[3]),
+            height: parseInt(params[4])
+        };
+    }
+    return null;
+}
+
+function parseUrl(url) {
+    return decodeURI(url);
+}
+
+function parseHeaders(headers) {
+    const res = qs.parse(headers, {
+        delimiter: ';'
+    });
+    return _.isEmpty(res) ? null : res;
+}
+
+
+/* Options reader */
+
+function readOptions(data, schema) {
+    const keys = _.keys(schema.describe().children),
+          options = _.pick(data, keys);
+
+    options.url = parseUrl(options.url);
+    options.headers = parseHeaders(options.headers);
+    options.clipRect = parseClipRect(options.clipRect);
+
+    return _.omitBy(options, (v) => _.isUndefined(v) || _.isNull(v));
+}
+
+
+/* Exported functions */
+
+module.exports = {
+    createSchema: createSchema,
+    readOptions: readOptions
+};
--- src/routes.js
+++ src/routes.js
@@ -1,97 +1,14 @@
 "use strict";
 
 const _ = require('lodash'),
-      qs = require('qs'),
       fs = require('fs-extra'),
-      joi = require('joi'),
       util = require('util'),
       logger = require('winston'),
       request = require('request'),
       UrlPattern = require('url-pattern'),
       capture = require('./capture'),
       utils = require('./utils'),
-
-      REGEXP_CLIP_RECT = /^(\d*),(\d*),([1-9]\d*),([1-9]\d*)$/;
-
-
-/* Schemas */
-
-function createSchema() {
-    return joi.object().keys({
-        force: joi.boolean(),
-        url: joi.string().trim().required(),
-        agent: joi.string().trim(),
-        headers: joi.string().trim(),
-        delay: joi.number().integer().min(0),
-        format: joi.string().lowercase().trim().valid(
-            'jpeg', 'jpg', 'png', 'html', 'bmp', 'pdf', 'ppm', 'ico'),
-        engine: joi.string().lowercase().trim().valid(
-            'phantomjs', 'slimerjs'),
-        quality: joi.number().min(0).max(1),
-        width: joi.number().integer().min(1),
-        height: joi.number().integer().min(1),
-        clipRect: joi.string().trim().regex(REGEXP_CLIP_RECT),
-        zoom: joi.number().min(0),
-        selector: joi.string().trim(),
-        js: joi.boolean(),
-        images: joi.boolean(),
-        user: joi.string().trim(),
-        password: joi.string().trim(),
-        callback: joi.string().trim(),
-        cookies: joi.array().items(
-            joi.object().keys({
-                name: joi.string().required(),
-                value: joi.string().required(),
-                domain: joi.string(),
-                path: joi.string().required(),
-                httponly: joi.boolean(),
-                secure: joi.boolean(),
-                expires: joi.string()
-            })
-        )
-    });
-}
-
-
-/* Functions to parse options */
-
-function parseClipRect(cr) {
-    const params = (cr || '').match(REGEXP_CLIP_RECT);
-    if (params && (params.length === 5)) {
-        return {
-            top: parseInt(params[1]),
-            left: parseInt(params[2]),
-            width: parseInt(params[3]),
-            height: parseInt(params[4])
-        };
-    }
-    return null;
-}
-
-function parseUrl(url) {
-    return decodeURI(url);
-}
-
-function parseHeaders(headers) {
-    const res = qs.parse(headers, {
-        delimiter: ';'
-    });
-    return _.isEmpty(res) ? null : res;
-}
-
-
-/* Options reader */
-
-function readOptions(data, schema) {
-    const keys = _.keys(schema.describe().children),
-          options = _.pick(data, keys);
-
-    options.url = parseUrl(options.url);
-    options.headers = parseHeaders(options.headers);
-    options.clipRect = parseClipRect(options.clipRect);
-
-    return _.omitBy(options, (v) => _.isUndefined(v) || _.isNull(v));
-}
+      opt = require('./options');
 
 
 /* Utility functions */
@@ -185,13 +102,13 @@ function sendImageToUrl(res, config, options) {
 
 function index(config) {
     return (req, res) => {
-        const schema = createSchema(),
+        const schema = opt.createSchema(),
               data = utils.validate(req.data, schema);
 
         if (data.error) {
             res.json(error(data.error.details));
         } else {
-            const options = readOptions(data.value, schema),
+            const options = opt.readOptions(data.value, schema),
                   siteUrl = options.url;
 
             if (!isUrlAllowed(config, siteUrl)) {
